bb645138941bdcd50492143195e83e7c08cb50b8
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-21 08:45:37 +0530

    toast completed

--- app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
+++ app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
@@ -3,6 +3,9 @@ package com.durgaprasad.androidplayground;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view){
+        EditText editText = (EditText) findViewById(R.id.PersonName);
+        Toast.makeText(this, "hello "+editText.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
