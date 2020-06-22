b99984f3b70a4de075244a60292a67ae314dd59a
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-22 13:23:25 +0530

    images completed

--- app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
+++ app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
@@ -3,11 +3,12 @@ package com.durgaprasad.androidplayground;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void buttonClicked(View view){
-        EditText editText = (EditText) findViewById(R.id.PersonName);
-        Toast.makeText(this, "hello "+editText.getText().toString(), Toast.LENGTH_LONG).show();
+    public void changeImageBtnclicked(View v){
+        ImageView imageView = (ImageView) findViewById(R.id.catimageView);
+        if ("catImage".equals(imageView.getTag())) {
+            imageView.setImageResource(R.drawable.tiger);
+            imageView.setTag("tigerImage");
+        }
+        else {
+            imageView.setImageResource(R.drawable.cat);
+            imageView.setTag("catImage");
+        }
     }
+
 }
