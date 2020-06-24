ba63c94c2e11fc50963af5004bb3a35baf7392ca
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-24 18:17:44 +0530

    basic play pause completed

--- app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
+++ app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
@@ -2,13 +2,25 @@ package com.durgaprasad.androidplayground;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioPlaybackConfiguration;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this,R.raw.heavy_rain);
+    }
+    public void play(View view){
+        mediaPlayer.start();
+    }
+    public void pause(View view){
+        mediaPlayer.pause();
     }
 }
