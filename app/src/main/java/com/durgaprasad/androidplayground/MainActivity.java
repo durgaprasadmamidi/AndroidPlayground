6272026cb330b15167c128e69090c0e5ba0eaab5
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-25 15:12:45 +0530

    ListViews completed

--- app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
+++ app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
@@ -3,6 +3,16 @@ package com.durgaprasad.androidplayground;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ArrayList<String> family = new ArrayList<String>(asList("Dp","nancy","Michel","Elle"));
+
+        ListView myListView = findViewById(R.id.myListView);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,family);
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), family.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
