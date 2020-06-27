5e8645d8c3cabbc8bb31ad61d284d9d371aa91e2
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-27 12:20:03 +0530

    Spinner completed

--- app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
+++ app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
@@ -3,12 +3,43 @@ package com.durgaprasad.androidplayground;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.myListView);
+        spinner = findViewById(R.id.citySpinner);
+        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList("dp","nancy","jack","Elle"));
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, spinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+
 }
