1433f4455194b223cc59abbd434f2457aa8af349
durga prasad <durgaprasad.mamidi1608@gmail.com>
2020-06-28 14:26:48 +0530

    Material Card View completed

--- app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
+++ app/src/main/java/com/durgaprasad/androidplayground/MainActivity.java
@@ -3,12 +3,26 @@ package com.durgaprasad.androidplayground;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialCardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView = findViewById(R.id.cardView);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Snack Bar",BaseTransientBottomBar.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
